02dd1388f3b0cd0fa9a4459620d560fea5908bb2
Inbal <inbaltsa@post.bgu.ac.il>
2026-01-13 15:18:21 +0200

    שיעורי בית

---
+++ homework_data.js
@@ -0,0 +1,5 @@
+const homeworkData = [
+    { "id": "1", "grade": 5, "subject": "אנגלית", "task": "Plurals p. w. b. p. 66-68 " },
+    { "id": "2", "grade": 5, "subject": "מתמטיקה", "task": "8-13 " },
+    { "id": "3", "grade": 5, "subject": "אנגלית", "task": "Plurals p. w. b. p. 66-68 " }
+];
