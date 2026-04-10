59ac545ab516c76eb17cfd402654831d3c6aa262
Inbal <inbaltsa@post.bgu.ac.il>
2026-04-10 12:21:34 +0300

    firebase

--- homework_data.js
+++ homework_data.js
@@ -1,1 +1,9 @@
-const homeworkData = [];
+const homeworkData = [
+    {
+        "id": "test1",
+        "grade": 3,
+        "subject": "חשבון",
+        "task": "בדיקה שהמערכת עובדת",
+        "date": "10/04"
+    }
+];
